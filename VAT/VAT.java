1eb5b66a63a3ecc9cb7f224db041563b9043620c
Sergii Gutsuliak <wheelyhog@gmail.com>
2022-10-15 11:11:46 +0200

    add project Java-Additional

---
+++ VAT/VAT.java
@@ -0,0 +1,59 @@
+package VAT;
+
+import java.util.Scanner;
+
+public class VAT {
+    private static double productPrice;
+    private static int productQuantity;
+    private static char symbl;
+
+    static Scanner scanner = new Scanner(System.in);
+
+    static void getQuantity() {
+        System.out.println("Enter quantity of products ...");
+        productQuantity = scanner.nextInt();
+    }
+
+    static void getPrice() {
+        double result;
+        System.out.println("Enter product price, EUR ...");
+        result = (scanner.nextDouble()) * 1.2;
+        if (result == (int) result) {
+            productPrice = result;
+        } else {
+            productPrice = (int) result + 1;
+        }
+        System.out.println("productprice = " + productPrice);
+
+    }
+
+    static double getResult() {
+
+        double result;
+        result = productPrice * productQuantity;
+        return result;
+    }
+
+    static void oneIteration() {
+        double sum = getResult();
+        getQuantity();
+        getPrice();
+        getResult();
+        System.out.println("PriceOfAll = " + (sum + getResult()));
+        System.out.println("Do yo want enter data for another product group? y/n ");
+        symbl = scanner.next().charAt(0);
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        oneIteration();
+        while (symbl == 'y') {
+            oneIteration();
+
+        }
+    }
+
+}
